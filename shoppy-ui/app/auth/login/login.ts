184aa107af809ba168283f793b32c1f16a77514e
anmolcnc <anmol@curvesncolors.ch>
2024-08-27 19:58:41 +0545

    login ui

---
+++ shoppy-ui/app/auth/login/login.ts
@@ -0,0 +1,20 @@
+"use server";
+
+import { FormError } from "@/app/common/form-error.interface";
+import { API_URL } from "@/app/constants/api";
+import { getErrorMessage } from "@/app/utils/errors";
+import { redirect } from "next/navigation";
+
+export default async function login(_prevState: FormError, formData: FormData) {
+  const res = await fetch(`${API_URL}/auth/login`, {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify(Object.fromEntries(formData)),
+  });
+  const parsedRes = await res.json();
+  if (!res.ok) {
+    return { error: getErrorMessage(parsedRes) };
+  }
+
+  redirect("/");
+}
--- shoppy-ui/app/auth/signup/create-user.ts
+++ shoppy-ui/app/auth/signup/create-user.ts
@@ -1,11 +1,12 @@
 "use server";
 
+import { FormError } from "@/app/common/form-error.interface";
 import { API_URL } from "@/app/constants/api";
 import { getErrorMessage } from "@/app/utils/errors";
 import { post } from "@/app/utils/fetch";
 import { redirect } from "next/navigation";
 
-export default async function createUser(_prevState: any, formData: FormData) {
+export default async function createUser(_prevState: FormError, formData: FormData) {
   const { error } = await post("users", formData);
   if (error) {
     return { error };
